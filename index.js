c93bdd872939345d6c90420236bfd2bc31541fda
leon <de.nizard@hotmail.com>
2023-04-06 16:41:31 -0300

    criado funcionalidade para ordenar produtos de [A-Z]

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const section = document.querySelector('section')
 const tableBody = document.querySelector('table tbody')
 const inputSearch = document.querySelector('#search')
 const btnFilter = document.querySelector('#filter')
+//div que contem as opçoes do filtro que contem os radioButtons
 const divOpcoes = document.querySelector('.opcoes')
+//radio button [A-Z]
+const radioOrdenarAz = document.querySelector('#ordenarAz')
 
 async function loadData(){
     try{
@@ -56,10 +59,13 @@ function converteData(data){
     return dataFormatada
 }
 
+//funçao respomsável por filtrar os dados input adcionando na query e filtrando pelo nome, transformado tudo em minusculo para não haver problemas de camelCase entre o array e os dados digitados
+
 function searchProduto(produtos, query){
     //console.log(produtos, query)
     return produtos.filter(produtos => produtos.nome.toLowerCase().includes(query.toLowerCase()))
 }
+//função que recebe o json e filtra os produtos adcionando em um novo array que inserido na tabela atualizando dinamicamente
 
 async function handleInputChange(){
     const response = await fetch('produtos.json')
@@ -81,17 +87,46 @@ async function handleInputChange(){
 }
 
 inputSearch.addEventListener('input', handleInputChange)
-
 loadData()
 
-
+//evento que abri ou fecha a div opçoes pelo método toggle
 btnFilter.addEventListener('click', event => {
     event.stopPropagation();
     divOpcoes.classList.toggle('active');
 });
-  
+//evento que remove as opçoes ao clicar no body
 document.body.addEventListener('click', event => {
     if (!divOpcoes.contains(event.target)) {
       divOpcoes.classList.remove('active');
     }
 });
+
+
+//function que orderna o produto organizando de a-z
+
+async function ordenarAz() {
+    const response = await fetch('produtos.json');
+    const data = await response.json();
+  
+    const produtos = data.produtos;
+  
+    produtos.sort((a, b) => {
+      if (a.nome < b.nome) {
+        return -1;
+      }
+      if (a.nome > b.nome) {
+        return 1;
+      }
+      return 0;
+    });
+
+    let html = ''
+
+    for(const produto of produtos){
+      
+        html += `<tr><td>${produto.nome}</td><td>R$: ${produto.preco_compra},00</td><td>R$: ${produto.preco_venda},00</td><td>${converteData(produto.data_compra)}</td></tr>`
+    }
+
+    tableBody.innerHTML = html
+    // console.log(produtos);
+  }
